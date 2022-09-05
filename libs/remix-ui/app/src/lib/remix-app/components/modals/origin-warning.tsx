d3148c430c78e2b4a33e0d95115b9f306304c4bb
turkeydevzone <107776416+turkeydevzone@users.noreply.github.com>
2022-09-04 23:49:34 -0300

    Update origin-warning.tsx

--- libs/remix-ui/app/src/lib/remix-app/components/modals/origin-warning.tsx
+++ libs/remix-ui/app/src/lib/remix-app/components/modals/origin-warning.tsx
@@ -1,1 +1,23 @@
 
+import React, { useEffect, useState } from 'react'
+import { ModalDialog } from '@remix-ui/modal-dialog'
+import { useDialogDispatchers } from '../../context/provider'
+
+const OriginWarning = () => {
+  const { alert } = useDialogDispatchers()
+  const [content, setContent] = useState<string>(null)
+
+  useEffect(() => {
+      setContent('Welcome to Remix')
+  }, [])
+
+  useEffect(() => {
+    if (content) {
+      alert({ id: 'warningOriging', title: null, message: content })
+    }
+  }, [content])
+
+  return (<></>)
+}
+
+export default OriginWarning
